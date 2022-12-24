595a9d32da8070f4317141f2e2f4a66cdb53b50f
Thanh Nguyen <thanh.nguyen.minh@positivethinking.tech>
2022-12-24 20:23:00 +0700

    add test for ASPMVC2

---
+++ WebMVC2.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace WebMVC2.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 1);
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
